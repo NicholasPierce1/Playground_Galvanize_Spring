86d03aa27e4c4300c18bbf9cb0d12e8e385f370b
Nick Pierce <nickpierce7898@gmail.com>
2021-06-29 14:55:03 -0500

    IOC -- see below

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
+import org.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class HelloController {
@@ -13,7 +20,7 @@ public class HelloController {
         return "Hello from Spring!";
     }
 
-    @RequestMapping( value = "/paramStringBasic", produces = MediaType.TEXT_PLAIN_VALUE, method = RequestMethod.GET)
+    @RequestMapping( value = "/paramStringBasic", produces = MediaType.TEXT_PLAIN_VALUE, method = GET)
     public ResponseEntity<String> getParamStringBasic(
     ){
 
@@ -30,7 +37,7 @@ public class HelloController {
 
     }
 
-    @RequestMapping( value = "/paramString", produces = MediaType.TEXT_PLAIN_VALUE, method = RequestMethod.GET)
+    @RequestMapping( value = "/paramString", produces = MediaType.TEXT_PLAIN_VALUE, method = GET)
     public ResponseEntity<String> getParamString(
             @RequestParam(value = "first") String firstName,
             @RequestParam(value = "last", defaultValue = "Doe", required = false) String lastName,
@@ -50,7 +57,7 @@ public class HelloController {
 
     }
 
-    @RequestMapping( value ={ "/pathString/{first}/{last}", "/pathString/{first}/" }, produces = MediaType.TEXT_PLAIN_VALUE, method = RequestMethod.GET)
+    @RequestMapping( value ={ "/pathString/{first}/{last}", "/pathString/{first}", "/pathString/{first}/" }, produces = MediaType.TEXT_PLAIN_VALUE, method = GET)
     public ResponseEntity<String> getPathString(
             @PathVariable( value = "first" ) String firstName,
             @PathVariable( value = "last", required = false ) String last){
@@ -66,7 +73,7 @@ public class HelloController {
         );
     }
 
-    @RequestMapping( value = "/getN_Items", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+    @RequestMapping( value = "/getN_Items", method = GET, produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> getN_Items(
             @RequestParam(value = "n") int... firstName
     ){
@@ -82,6 +89,40 @@ public class HelloController {
                 HttpStatus.OK
         );
 
+    }
+
+    @RequestMapping(
+            value = {"/task/", "/task"},
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            method = RequestMethod.POST)
+    public Task postNewTask(@ModelAttribute("Task") Task task){
+        return task;
+    }
+
+    @RequestMapping(
+            value = {"/task/form", "/task/form/"},
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            method = RequestMethod.POST)
+    public Task postNewTaskWithForm(@ModelAttribute("Task") Task task){
+        return task;
+    }
+
+    @PostMapping(value = "/map-example", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String getMapParams(@RequestParam Map<String, String> formData) {
+        return formData.toString();
+    }
+
+    @RequestMapping(value = "/webRequest", method = GET)
+    public List<?> getAll(WebRequest webRequest){
+        Map<String, String[]> params = webRequest.getParameterMap();
+        // can get params & headers here
+
+        final ArrayList<String> list = new ArrayList<>();
+
+        //params.forEach( (x,y) -> {list.add(String.format("%s-%s"));} );
+
+        return null;
     }
 
 }
---
+++ src/main/java/com/example/demo/Task.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+public class Task {
+
+    public String name;
+    public String lastName;
+
+    public Task(){}
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
