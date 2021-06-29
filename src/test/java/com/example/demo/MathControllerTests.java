a74a2ea10c7164d94c5e2d584517b3144a0984ef
Nick Pierce <nickpierce7898@gmail.com>
2021-06-29 13:37:31 -0500

    initial commit

--- src/test/java/com/example/demo/MathControllerTests.java
+++ src/test/java/com/example/demo/MathControllerTests.java
@@ -6,6 +6,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.MultiValueMapAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -20,6 +26,20 @@ public class MathControllerTests {
     @Test
     public void testMathPi() throws Exception {
         this.mvc.perform(get("/math/pi").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk()).andExpect(content()
+                .string("3.141592653589793"));
+    }
+
+    @Test
+    public void testForm() throws Exception {
+        this.mvc.perform(get("/math/pi")
+                .params(new MultiValueMapAdapter<String, String>(
+                        new HashMap<String, List<String>>(){{
+                            put("first", new ArrayList<>(){{add("Nick");}});
+                            put("last", new ArrayList<>(){{add("Pierce");}});
+                        }}
+                ))
+                .accept(MediaType.TEXT_PLAIN))
                 .andExpect(status().isOk()).andExpect(content()
                 .string("3.141592653589793"));
     }
