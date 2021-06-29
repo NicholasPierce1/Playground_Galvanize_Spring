e558626abc38d40ae913ac48497934642d3f076c
Nick Pierce <nickpierce7898@gmail.com>
2021-06-29 10:04:57 -0500

    playground completed (no test yet) for request param, path, and headers

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -9,6 +9,59 @@ public class HelloController {
     @GetMapping("/")
     public String helloWorld() {
         return "Hello from Spring!";
+    }
+
+    @RequestMapping( value = "/paramStringBasic", produces = MediaType.TEXT_PLAIN_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<String> getParamStringBasic(
+    ){
+
+        // @RequestHeader( value = "testHeader", required = false, defaultValue = "middle") String middleName
+
+        // if required is false, then prepare to handle null if no default value
+        final HttpHeaders headers = new HttpHeaders();
+
+        return new ResponseEntity<String>(
+                "Hello",
+                headers,
+                HttpStatus.OK
+        );
+
+    }
+
+    @RequestMapping( value = "/paramString", produces = MediaType.TEXT_PLAIN_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<String> getParamString(
+            @RequestParam(value = "first") String firstName,
+            @RequestParam(value = "last", defaultValue = "Doe", required = false) String lastName,
+            @RequestHeader( value = "testHeader", required = false, defaultValue = "middle") String middleName
+            ){
+
+        // @RequestHeader( value = "testHeader", required = false, defaultValue = "middle") String middleName
+
+        // if required is false, then prepare to handle null if no default value
+        final HttpHeaders headers = new HttpHeaders();
+
+        return new ResponseEntity<String>(
+                "Hello, " + firstName + " " + middleName + " " + lastName,
+                headers,
+                HttpStatus.OK
+        );
+
+    }
+
+    @RequestMapping( value ={ "/pathString/{first}/{last}", "/pathString/{first}/" }, produces = MediaType.TEXT_PLAIN_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<String> getPathString(
+            @PathVariable( value = "first" ) String firstName,
+            @PathVariable( value = "last", required = false ) String last){
+
+        final HttpHeaders headers = new HttpHeaders();
+
+        final String lastName = last == null ? "Doe" : last;
+
+        return new ResponseEntity<String>(
+                "Hello, " + firstName + " " + lastName,
+                headers,
+                HttpStatus.OK
+        );
     }
 
 }
