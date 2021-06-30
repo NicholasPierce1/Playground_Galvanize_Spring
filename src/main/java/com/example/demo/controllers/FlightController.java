e88c79255a748b5903f7ccecacd3190cf5af143f
Nick Pierce <nickpierce7898@gmail.com>
2021-06-30 15:58:38 -0500

    all test passed -- forgot to make a commit for each since I was multitasking!

--- src/main/java/com/example/demo/controllers/FlightController.java
+++ src/main/java/com/example/demo/controllers/FlightController.java
@@ -1,4 +1,41 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.FlightTickets;
+import com.example.demo.model.Ticket;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping(value = "/flights/tickets/")
 public class FlightController {
+
+    @RequestMapping(
+            value = {"/total", "/total/"},
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            method = RequestMethod.POST
+    )
+    public ResponseEntity<String> postCalculateTotalTicketCost(@RequestBody FlightTickets flightTickets) throws Exception{
+
+        JSONObject jsonObject = new JSONObject();
+
+        double price = 0;
+
+        for(final Ticket ticket : flightTickets.getTickets())
+            price += ticket.getPrice();
+
+        jsonObject.put("result", price);
+
+        return new ResponseEntity<String>(jsonObject.toString(), HttpStatus.OK);
+
+    }
+
 }
--- src/test/java/com/example/demo/FlightJsonTest.java
+++ src/test/java/com/example/demo/FlightJsonTest.java
@@ -9,7 +9,10 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -24,6 +27,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 // import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,46 +39,34 @@ import static org.hamcrest.Matchers.is;
 
 
 @WebMvcTest(FlightController.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FlightJsonTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc _mvc;
 
-    @Test
-    public void testGetTicketPriceSum() throws Exception{
+    private FlightTickets _flightTickets;
 
-        // step 1: create JSON data for input and output/expected (String)c
-        final Gson jsonConverter = new GsonBuilder().setPrettyPrinting().create();
+    private FlightTickets _flightTicketsCompatibleWithGson;
 
-        final ObjectMapper objectMapper = JsonMapper.builder()
+    private ObjectMapper _objectMapper;
+
+    private Gson _gson;
+
+    @BeforeAll
+    public void setConstantState(){
+
+        this._gson = new GsonBuilder().setPrettyPrinting().create();
+
+        this._objectMapper = JsonMapper.builder()
                 .findAndAddModules()
                 .build();
 
-        final FlightTickets flightTickets = new FlightTickets();
+    }
 
-        final Passenger passengerOne = new Passenger();
-        passengerOne.setFirstName("Some name");
-        passengerOne.setLastName("Some other name");
-
-        final Passenger passengerTwo = new Passenger();
-        passengerTwo.setFirstName("Name B");
-        passengerTwo.setLastName("Name C");
-
-        final Ticket ticketOne = new Ticket();
-        ticketOne.setPrice(200);
-        ticketOne.setPassenger(passengerOne);
-
-        final Ticket ticketTwo = new Ticket();
-        ticketTwo.setPrice(150);
-        ticketTwo.setPassenger(passengerTwo);
-
-        flightTickets.setTickets(new Ticket[]{ticketOne, ticketTwo});
-
-        // create expected
-        final JSONObject expected = new JSONObject();
-        expected.put("result", 350);
-
-        final FlightTickets flightTickets1 = new FlightTickets(){
+    @BeforeEach
+    public void setMemberState(){
+        this._flightTickets = new FlightTickets(){
             {
                 setTickets(
                         new Ticket[]{
@@ -98,11 +94,42 @@ public class FlightJsonTest {
                 );
             }};
 
-        System.out.println( objectMapper.writeValueAsString(flightTickets1) );
+        this._flightTicketsCompatibleWithGson = new FlightTickets();
 
-        final String jsonString = jsonConverter.toJson(flightTickets, FlightTickets.class);
+        final Passenger passengerOne = new Passenger();
+        passengerOne.setFirstName("Some name");
+        passengerOne.setLastName("Some other name");
 
+        final Passenger passengerTwo = new Passenger();
+        passengerTwo.setFirstName("Name B");
+        passengerTwo.setLastName("Name C");
+
+        final Ticket ticketOne = new Ticket();
+        ticketOne.setPrice(200);
+        ticketOne.setPassenger(passengerOne);
+
+        final Ticket ticketTwo = new Ticket();
+        ticketTwo.setPrice(150);
+        ticketTwo.setPassenger(passengerTwo);
+
+        this._flightTicketsCompatibleWithGson.setTickets(new Ticket[]{ticketOne, ticketTwo});
+
+
+    }
+
+    @Test
+    public void testGetTicketPriceSumSerialize() throws Exception{
+
+        // step 1: create JSON data for input and output/expected (String)
+
+        // create expected
+        final JSONObject expected = new JSONObject();
+        expected.put("result", 350);
+
+        final String jsonString = this._gson.toJson(this._flightTicketsCompatibleWithGson, FlightTickets.class);
         assertTrue(true);
+        System.out.println(jsonString);
+        // System.out.println(this._objectMapper.writeValueAsString(this._flightTickets));
 
         // step 2: create post request & tailor the json data to it
 
@@ -111,12 +138,86 @@ public class FlightJsonTest {
                 .content(jsonString)
                 .accept(MediaType.APPLICATION_JSON_VALUE);
 
-        this.mvc.perform(postRequest)
+        this._mvc.perform(postRequest)
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.result").value(expected.getDouble("result")))
+                //.andExpect(jsonPath("$.result").value(expected.getDouble("result")))
                 .andExpect(jsonPath("$.result", is(350)));
                 //.andExpect(jsonPath("$").value(expected.toString()));
                 //.andExpect(content().json(expected.toString()));
+        //                .andExpect((ResultMatcher) jsonPath("$.result", is(expected.get("result"))));
+
+        // step 3: create, and run, mvc test and assert json object is expected
+
+    }
+
+    @Test
+    public void testGetTicketPriceSumStringLiteral() throws Exception{
+
+        // step 1: create JSON data for input and output/expected (String)
+
+        // create expected
+        final JSONObject expected = new JSONObject();
+        expected.put("result", 350);
+
+        final String jsonString =
+                "{\"tickets\":[{\"passenger\":{\"firstName\":\"Some name\"," +
+                        "\"lastName\":\"Some other name\"},\"price\":200.0},{" +
+                        "\"passenger\":{\"firstName\":\"Name B\"," +
+                        "\"lastName\":\"Name C\"},\"price\":150.0}]}";
+        assertTrue(true);
+        // System.out.println(this._objectMapper.writeValueAsString(this._flightTickets));
+
+        // step 2: create post request & tailor the json data to it
+
+        MockHttpServletRequestBuilder postRequest = post("/flights/tickets/total")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(jsonString)
+                .accept(MediaType.APPLICATION_JSON_VALUE);
+
+        this._mvc.perform(postRequest)
+                .andExpect(status().isOk())
+                //.andExpect(jsonPath("$.result").value(expected.getDouble("result")))
+                .andExpect(jsonPath("$.result", is(350)));
+        //.andExpect(jsonPath("$").value(expected.toString()));
+        //.andExpect(content().json(expected.toString()));
+        //                .andExpect((ResultMatcher) jsonPath("$.result", is(expected.get("result"))));
+
+        // step 3: create, and run, mvc test and assert json object is expected
+
+    }
+
+    @Test
+    public void testGetTicketPriceSumJsonResourceFile() throws Exception{
+
+        // step 1: create JSON data for input and output/expected (String)
+
+        // create expected
+        final JSONObject expected = new JSONObject();
+        expected.put("result", 350);
+
+        final URL jsonResourceLocation = this.getClass().getResource("/flightTickets.json");
+
+        assertNotNull(jsonResourceLocation, "json path resource location is null.");
+
+        File f = new File(jsonResourceLocation.getPath());
+
+        final String jsonString = Files.readString(f.toPath());
+
+        // System.out.println(this._objectMapper.writeValueAsString(this._flightTickets));
+
+        // step 2: create post request & tailor the json data to it
+
+        MockHttpServletRequestBuilder postRequest = post("/flights/tickets/total")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(jsonString)
+                .accept(MediaType.APPLICATION_JSON_VALUE);
+
+        this._mvc.perform(postRequest)
+                .andExpect(status().isOk())
+                //.andExpect(jsonPath("$.result").value(expected.getDouble("result")))
+                .andExpect(jsonPath("$.result", is(350)));
+        //.andExpect(jsonPath("$").value(expected.toString()));
+        //.andExpect(content().json(expected.toString()));
         //                .andExpect((ResultMatcher) jsonPath("$.result", is(expected.get("result"))));
 
         // step 3: create, and run, mvc test and assert json object is expected
