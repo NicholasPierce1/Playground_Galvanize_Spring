fed126b982cb9ab415e0c03bfbe43ae1b4c48de2
Nick Pierce <nickpierce7898@gmail.com>
2021-07-01 13:46:30 -0500

    custom operations completed

--- src/main/java/com/example/demo/Repository/LessonRepository.java
+++ src/main/java/com/example/demo/Repository/LessonRepository.java
@@ -1,9 +1,32 @@
 package com.example.demo.Repository;
 
 import com.example.demo.domain.Lesson;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface LessonRepository extends CrudRepository<Lesson, Long> {
+
+    public Lesson findByName(String name);
+
+    //@Query(value = "Select l from Lesson l where delivered_on >= ?1 AND delivered_on <= ?2")
+    public List<Lesson> findByDeliveredOnBetween(LocalDate start, LocalDate end);
+
+    @Query(value = "select * " +
+            "from Lesson " +
+            "where " +
+            "delivered_on >= to_date('?1-?2-?3', 'yyyy-MM-dd') " +
+            "AND " +
+            "delivered_on <= to_date('?4-?5-?6', 'yyyy-MM-dd')" +
+            ";", nativeQuery = true)
+    public List<Lesson> findByDeliveredOnBetweenDates(
+            String startYear, int startMonth, int startDay,
+            int endYear, int endMonth, int endDay
+    );
+
 }
--- src/main/java/com/example/demo/controllers/LessonController.java
+++ src/main/java/com/example/demo/controllers/LessonController.java
@@ -6,6 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class LessonController {
 
@@ -49,6 +55,41 @@ public class LessonController {
         lessonToUpdate.setDeliveredOn(lesson.getDeliveredOn());
         lessonToUpdate.setName(lesson.getName());
         return this._lessonRepository.save(lessonToUpdate);
+    }
+
+    @GetMapping(
+            value = "/lesson/find/{title}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Lesson getLessonByTitle(@PathVariable String title){
+        return this._lessonRepository.findByName(title);
+    }
+
+    @GetMapping(
+            value = "/lesson/between",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Lesson> getLessonByDateRange(
+            @RequestParam(value = "startDate") String startDateString,
+            @RequestParam(value = "endDate") String endDateString
+            ){
+
+        final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        final LocalDate startDate = LocalDate.parse(startDateString, dateFormatter);
+        final LocalDate endDate = LocalDate.parse(endDateString, dateFormatter);
+
+        return this._lessonRepository.findByDeliveredOnBetween(startDate, endDate);
+    }
+
+    @GetMapping(
+            value = {"/lesson", "/lesson/"},
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Lesson> getLessons(){
+        final List<Lesson> lessons = new ArrayList<Lesson>();
+        this._lessonRepository.findAll().forEach( lessons::add );
+        return lessons;
     }
 
 }
