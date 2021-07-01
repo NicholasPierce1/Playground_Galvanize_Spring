b910b511824710fcad02cd67cf43caca2437e441
Nick Pierce <nickpierce7898@gmail.com>
2021-07-01 14:38:47 -0500

    tests for custom operations completed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 insert into Customer(id, first_name, last_name) values (1, 'John', 'Doe');
 insert into Customer(id, first_name, last_name) values (2, 'Jane', 'Doe');
 
-insert into Lesson(id, name, delivered_on) values (1, 'Spring', TO_DATE('2021-6-30', 'yyyy-MM-dd'));
-insert into Lesson(id, name, delivered_on) values (2, 'Angular', TO_DATE('2021-7-7', 'yyyy-MM-dd'));
+insert into Lesson(id, name, delivered_on) values (1, 'Spring', TO_DATE('2021-06-30', 'yyyy-MM-dd'));
+insert into Lesson(id, name, delivered_on) values (2, 'Angular', TO_DATE('2021-07-07', 'yyyy-MM-dd'));
--- src/test/java/com/example/demo/LessonTests.java
+++ src/test/java/com/example/demo/LessonTests.java
@@ -4,6 +4,8 @@ import com.example.demo.Repository.LessonRepository;
 import com.example.demo.domain.Lesson;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -40,11 +44,15 @@ public class LessonTests {
 
     private ObjectMapper _objectMapper;
 
+    private DateTimeFormatter _dateTimeFormatter;
+
     @BeforeAll
     public void setInstanceState(){
         this._objectMapper = JsonMapper.builder()
                 .findAndAddModules()
                 .build();
+
+        this._dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     }
 
     @Test
@@ -68,7 +76,7 @@ public class LessonTests {
                         jsonPath(
                                 "$.deliveredOn",
                                 is(lesson.getDeliveredOn().format(
-                                        DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                                        this._dateTimeFormatter
                                 ))
                         )
                 )
@@ -115,6 +123,68 @@ public class LessonTests {
 
     }
 
-    
+    @Test
+    @Transactional
+    @Rollback
+    public void testFindByName() throws Exception{
+
+        final long id = 1;
+
+        final Lesson expected = new Lesson(){{
+            setName("Spring");
+            setDeliveredOn(LocalDate.parse("2021-06-30", _dateTimeFormatter));
+            setId(id);
+        }};
+
+        final MockHttpServletRequestBuilder getByName = get("/lesson/find/" + expected.getName());
+
+        this._mvc.perform(getByName)
+                .andExpect(status().isOk())
+                .andExpect(content().json(this._objectMapper.writeValueAsString(expected)));
+
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testGetDateBetween() throws Exception{
+
+        final long idOne = 1;
+        final long idTwo = 2;
+
+        final Lesson expectedOne = new Lesson(){{
+            setName("Spring");
+            setDeliveredOn(LocalDate.parse("2021-06-30", _dateTimeFormatter));
+            setId(idOne);
+        }};
+
+        final Lesson expectedTwo = new Lesson(){{
+            setName("Angular");
+            setDeliveredOn(LocalDate.parse("2021-07-07", _dateTimeFormatter));
+            setId(idTwo);
+        }};
+
+        final JSONArray expectedJsonArray = new JSONArray();
+        expectedJsonArray.put(this._objectMapper.writeValueAsString(expectedOne));
+        expectedJsonArray.put(this._objectMapper.writeValueAsString(expectedTwo));
+
+        final MockHttpServletRequestBuilder getBetweenDates = get(
+                "http://localhost:8080/lesson/between?startDate=2000-01-01&endDate=2022-12-20"
+        );
+
+        final MockHttpServletResponse response = this._mvc.perform(getBetweenDates)
+                .andExpect(status().isOk()).andReturn().getResponse();
+
+        final JSONArray actualJsonArray = new JSONArray(response.getContentAsString());
+
+        for(int i = 0; i < actualJsonArray.length(); i++){
+
+            final JSONObject expectedJsonObject = new JSONObject(expectedJsonArray.getString(i));
+            final JSONObject actualJsonObject = new JSONObject(actualJsonArray.getString(i));
+
+            assertEquals(actualJsonObject.toString(), expectedJsonObject.toString());
+        }
+
+    }
 
 }
