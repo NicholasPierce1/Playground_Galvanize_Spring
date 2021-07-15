b80bd34f77892ede21e8b1f3cfad8df204949139
Nicholas Pierce <nickpierce7898@gmail.com>
2021-07-15 14:08:43 -0500

    kafka

---
+++ src/main/java/com/example/demo/controllers/KafkaProducerController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controllers;
+
+import com.example.demo.service.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka/producer/")
+public class KafkaProducerController {
+
+    private final KafkaProducer producer;
+
+    @Autowired
+    public KafkaProducerController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping("/publish")
+    public boolean messageToTopic(
+            @RequestParam( value= "message", required = false, defaultValue = "hello from kafka producer") String message){
+
+        this.producer.sendMessage(message);
+
+        return true;
+    }
+
+}
---
+++ src/main/java/com/example/demo/service/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "test_topic",groupId = "group_id")
+    public void consumeMessage(String message){
+
+        System.out.println(message);
+    }
+
+}
